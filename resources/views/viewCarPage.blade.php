eca5f7dea2431c6da7887530813619c1f2ce0cc9
EivonneGoh <eivonnegohyeefang@1utar.my>
2025-04-18 12:22:13 +0800

    RBAC3

--- resources/views/viewCarPage.blade.php
+++ resources/views/viewCarPage.blade.php
@@ -4,11 +4,11 @@
 
 @section('content')
 
-
+@can('isAdmin')
 <div class="wedo" style="display: flex; justify-content: flex-end;padding-right:50px; padding-top:50px; ">
     <button style="width:fit-content;"onclick="window.location.href='{{ route('updateCar') }}'">Update Car Inventory</button>
     </div>
-
+@endcan
     <!-- Brand Buttons -->
     <div class="wedo" >
 </div>
